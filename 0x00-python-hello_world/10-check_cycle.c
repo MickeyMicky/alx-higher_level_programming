39d254effe0deff6fff2381d93f72041aab1c02a
MickeyMicky <mtchawa21@gmail.com>
2023-10-30 14:01:59 -0700

    compile

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,13 +10,13 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow = list->next;
+	listint_t *fast = list->next->next;
 
-	if (list == NULL)
+	if (list == NULL || list-next == NULL)
 		return (0);
 
-	while (fast != NULL && fast->next != NULL)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
