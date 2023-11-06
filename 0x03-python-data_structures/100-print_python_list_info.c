000d7de32ea8353e6c5bf31728cbc652f0cd4b7b
MickeyMicky <mtchawa21@gmail.com>
2023-11-06 12:41:56 -0800

    14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,26 @@
+#include "Python.h"
+#include <stdlib.h>
+
+/**
+ * print_python_list_info - C function that prints some
+ *                          basic info about Python lists.
+ * @p: pointer to generic PyObject
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *py_list = NULL;
+	ssize_t l_len = 0;
+	ssize_t i = 0;
+	const char *ele_type = NULL;
+
+	l_len = PyList_Size(p);
+	py_list = (PyListObject *)p;
+	printf("[*] Size of the Python List = %ld\n", l_len);
+	printf("[*] Allocated = %ld\n", (signed long)(py_list->allocated));
+	while (i < l_len)
+	{
+		ele_type = Py_TYPE(py_list->ob_item[i])->tp_name;
+		printf("Element %ld: %s\n", i, ele_type);
+		i++;
+	}
+}
