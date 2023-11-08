9cfe598a22ce57d09873671f145cb87b609e8b92
MickeyMicky <mtchawa21@gmail.com>
2023-11-08 04:54:30 -0800

    0 to 6

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,5 @@ def uniq_add(my_list=[]):
     """
     function that adds all unique integers
     in a list (only once for each integer).
-    """
-    
+    """    
     return (sum({ele for ele in my_list}))
